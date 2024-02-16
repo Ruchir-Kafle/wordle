9f44797c227576de9c5f9e81ec67e06d21a01516
Ruchir <Ruchir@webdev15.local>
2024-02-16 11:05:04 -0500

    board

--- app.js
+++ app.js
@@ -8,40 +8,64 @@ const keyboard = [
     "ZXCVBNM".split("")
 ]; // Defining the letters of the keyboard in three elements, each element representing a row on the keyboard and each element being an array.
 keyboard[2].unshift("ENTER");
-keyboard[2].push("BACK")
+keyboard[2].push("⌫");
+
+let tries = [
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+];
+let attempt = 0;
 
 // Functions
 function logKey(evt) {
     if (evt.key.toLowerCase() == "enter") { // Triggers when the enter key is pressed
         console.log("Submit"); // Prints "Submit" in the console
+        if (attempt < 6 && tries[attempt].length == 5) {
+            attempt++;
+        };
     } else if (evt.key.toLowerCase() == "backspace") { // Triggers when the backspace key is pressed
         console.log("Deleting"); // Prints "Deleting" in the console.
+        tries[attempt].pop();
     } else if (alpha.has(evt.key.toLowerCase())) { // Triggers when a key is pressed which is in the "alpha" set.
-        console.log(evt.key.toUpperCase()); // Prints the key pressed in the console.
+        if (tries[attempt].length < 5) {
+            tries[attempt].push(evt.key.toUpperCase());
+        };
     } else { // Triggers when a non-letter key is pressed.
         console.log("Key pressed is not a letter"); // Prints "Key pressed is not a letter" in the console.
     };
+
+    render();
 };
 
 function render() {
     // Searches the HTML document for an HTML tag with the "root" id.
-    const main = document.querySelector("#root"); 
-    let template = `<div class="keyboard">`;
+    const main = document.querySelector("#root");
+    let board = `<div class="board">`;
+
+    for (let i = 0; i < tries.length; i++) {
+        for (let j = 0; j < 5; j++) {
+            board += `<div> ${tries[i][j] ? tries[i][j] : ""}</div>`;
+        };
+    };
+    board += `</div>`;
+
+    let keyTemplate = `<div class="keyboard">`;
     
     for (let i = 0; i < keyboard.length; i++) {
-        template += `<div class = "row">`;
+        keyTemplate += `<div class = "row">`;
         for (let j = 0; j < keyboard[i].length; j++) {
-            template += `<div class="key">${keyboard[i][j]}</div>`;
-            console.log(j)
-        }
-        template += `</div>`
+            keyTemplate += `<div class="key">${keyboard[i][j]}</div>`;
+        };
+        keyTemplate += `</div>`;
     };
 
-    template += `</div>`
+    keyTemplate += `</div>`;
 
-    main.innerHTML = template;
-
-    console.log(main);
+    main.innerHTML = board + keyTemplate;
 }
 // Code
 
