621ef8d865a9aaaab9892ce34ea74ab2e72af5be
Ruchir <Ruchir@webdev15.local>
2024-03-04 10:34:36 -0500

    t

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Variables
-const secret = "aback"; // The word of the day.
+const secret = "ABACK"; // The word of the day.
 const letters = "abcdefghijklmnopqrstuvwxyz"; // All the letters in the alphabet.
 const alpha = new Set(letters.split("")); // Putting all the letters in the alphabet into a set.
 const keyboard = [
@@ -22,54 +22,93 @@ let attempt = 0;
 
 // Functions
 function logKey(evt) {
+    console.log(evt)
+    let matches = [0,0,0,0,0];
+
     if (evt.key.toLowerCase() == "enter") { // Triggers when the enter key is pressed
         console.log("Submit"); // Prints "Submit" in the console
+        
         if (attempt < 6 && tries[attempt].length == 5) {
+            matches = evaluate(tries[attempt]);
             attempt++;
         };
-    } else if (evt.key.toLowerCase() == "backspace") { // Triggers when the backspace key is pressed
+
+    } 
+    
+    else if (evt.key.toLowerCase() == "backspace") { // Triggers when the backspace key is pressed
         console.log("Deleting"); // Prints "Deleting" in the console.
         tries[attempt].pop();
-    } else if (alpha.has(evt.key.toLowerCase())) { // Triggers when a key is pressed which is in the "alpha" set.
+    } 
+    
+    else if (alpha.has(evt.key.toLowerCase())) { // Triggers when a key is pressed which is in the "alpha" set.
+        
         if (tries[attempt].length < 5) {
             tries[attempt].push(evt.key.toUpperCase());
         };
-    } else { // Triggers when a non-letter key is pressed.
+
+    } 
+    
+    else { // Triggers when a non-letter key is pressed.
         console.log("Key pressed is not a letter"); // Prints "Key pressed is not a letter" in the console.
     };
+    
 
-    render();
+    render(matches);
 };
 
-function render() {
+function render(matches) {
     // Searches the HTML document for an HTML tag with the "root" id.
     const main = document.querySelector("#root");
     let board = `<div class="board">`;
 
     for (let i = 0; i < tries.length; i++) {
+
         for (let j = 0; j < 5; j++) {
-            board += `<div> ${tries[i][j] ? tries[i][j] : ""}</div>`;
+            board += `<div class="match${matches[j]}">${tries[i][j] ? tries[i][j] : ""}</div>`;
         };
+
     };
+
     board += `</div>`;
 
     let keyTemplate = `<div class="keyboard">`;
     
     for (let i = 0; i < keyboard.length; i++) {
         keyTemplate += `<div class = "row">`;
+
         for (let j = 0; j < keyboard[i].length; j++) {
             keyTemplate += `<div class="key">${keyboard[i][j]}</div>`;
         };
+
         keyTemplate += `</div>`;
     };
 
     keyTemplate += `</div>`;
 
     main.innerHTML = board + keyTemplate;
-}
+};
+
+function evaluate(guess) {
+    let matches = [];
+
+    for (let i = 0; i < guess.length; i++) {
+        let found = 0;
+
+        if (guess[i] == secret[i]) {
+            found = 2;
+        } 
+        
+        else if (secret.includes(guess[i])) {
+            found = 1;
+        }
+
+        matches.push(found);
+    };
+    return matches;
+};
 // Code
 
-render();
+render([0,0,0,0,0]);
 
 // Events
 window.addEventListener("keyup", logKey); // Calling the function "logKey" whenever a key is released.
